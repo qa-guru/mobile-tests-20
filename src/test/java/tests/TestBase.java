3ccfff8d7d1b7294759c684045c2b7596dfb5b03
svasenkov <aanher@gmail.com>
2023-08-02 15:00:52 +0200

    android tests

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,40 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import drivers.BrowserstackDriver;
+import helpers.Attach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import io.qameta.allure.selenide.AllureSelenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = BrowserstackDriver.class.getName();
+        Configuration.browserSize = null;
+        Configuration.timeout = 30000;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open();
+    }
+
+    @AfterEach
+    void afterEach() {
+        String sessionId = sessionId().toString();
+
+//        Attach.screenshotAs("Last screenshot"); //todo
+        Attach.pageSource();
+
+        closeWebDriver();
+
+        Attach.addVideo(sessionId);
+    }
+}
